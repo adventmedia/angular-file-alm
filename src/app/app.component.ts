71bf47ebe1b1d3895ab3a101218c865a45411979
Acker Apple <ackerapple@Ackers-Mac-mini.lan>
2021-01-05 15:18:44 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,18 @@
 import { Component } from '@angular/core';
-
-//import { string as gettingStarted } from "./components/getting-started.template"
-import { string as template } from "./components/app.template"
-
 import {version} from 'angular-file/package.json'
 
 declare var PR: any;
 
 @Component({
   selector: 'app',
-  template: template
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   version:string = version
   //gettingStarted:string = gettingStarted;
-  
+
   ngAfterViewInit(){
+    console.log('angular-file version', version)
     setTimeout(()=>{
       if (typeof PR !== 'undefined') {
         // google code-prettify
